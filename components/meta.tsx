0941fbf10977f4310e2bacd1817904b04667f71d
Eric Crowder <eric@ebcrowder.dev>
2022-03-05 16:20:27 -0800

    Rm site.webmanifest

--- components/meta.tsx
+++ components/meta.tsx
@@ -9,7 +9,6 @@ const Meta = () => {
         sizes="32x32"
         href="/favicon/favicon.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
       <link rel="shortcut icon" href="/favicon/favicon.png" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
