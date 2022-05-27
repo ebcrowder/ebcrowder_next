dad6b9bbeba7018279ed27208301111a9db4a5a8
Eric Crowder <eric@ebcrowder.dev>
2022-05-27 08:48:04 -0700

    Update footer type signature and dep updates

--- components/container.tsx
+++ components/container.tsx
@@ -1,12 +1,11 @@
-import { ReactNode, FunctionComponent } from "react";
+import { ReactNode, FC } from "react";
 
 type Props = {
   children?: ReactNode;
 };
 
-const Container: FunctionComponent = ({ children }: Props) => {
+const Container: FC<Props> = ({ children }) => {
   return <div className="container mx-auto px-5">{children}</div>;
 };
-
 
 export default Container;
